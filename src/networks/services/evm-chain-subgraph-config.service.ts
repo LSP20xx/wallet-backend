07050f06c5e07271c3e25fc7d08e3c0275366f0a
Lautaro Perrotti <lautaroperrotti@gmail.com>
2023-11-10 20:48:43 -0300

    [feature] - add subgraphs logic

--- src/networks/services/evm-chain-subgraph-config.service.ts
+++ src/networks/services/evm-chain-subgraph-config.service.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 import * as path from 'path';
 import { EvmChainConfigService } from './evm-chain-config.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class EvmChainSubgraphConfigService {
-  constructor(private chainConfigService: EvmChainConfigService) {}
+  constructor(
+    private chainConfigService: EvmChainConfigService,
+    private configService: ConfigService,
+  ) {}
 
   generateSubgraphConfigs() {
     const chains = this.chainConfigService.allowedChains;
@@ -23,31 +27,42 @@ export class EvmChainSubgraphConfigService {
     name: string;
     rpcUrl: string;
   }): string {
+    const contractAddresses =
+      this.configService
+        .get(`${chain.name.toUpperCase()}_ERC20_CONTRACTS`)
+        ?.split(',') || [];
+
+    const dataSources = contractAddresses
+      .map(
+        (address: string) => `
+      - kind: ethereum/contract
+        name: ERC20Token
+        network: ${chain.name}
+        source:
+          address: "${address}"
+          abi: ERC20
+        mapping:
+          kind: ethereum/events
+          apiVersion: 0.0.5
+          language: wasm/assemblyscript
+          entities:
+            - Transfer
+          abis:
+            - name: ERC20
+              file: ./abis/ERC20.json
+          eventHandlers:
+            - event: Transfer(address,address,uint256)
+              handler: handleTransfer
+    `,
+      )
+      .join('');
+
     return `
       specVersion: 0.0.2
       description: Subgraph for ${chain.name}
       schema:
         file: ./schema.graphql
-      dataSources:
-        - kind: ethereum/contract
-          name: ERC20Token
-          network: ${chain.name}
-          source:
-            address: "<contract-address>"
-            abi: ERC20
-          mapping:
-            kind: ethereum/events
-            apiVersion: 0.0.5
-            language: wasm/assemblyscript
-            entities:
-              - Transfer
-            abis:
-              - name: ERC20
-                file: ./abis/ERC20.json
-            eventHandlers:
-              - event: Transfer(address,address,uint256)
-                handler: handleTransfer
-            file: ./mapping.ts
+      dataSources: ${dataSources}
     `;
   }
 }
