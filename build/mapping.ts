9f8e44a7f25aba618158d5836754140211199f53
Lautaro Perrotti <lautaroperrotti@gmail.com>
2023-12-21 01:10:49 -0300

    Initialize subgraph

---
+++ build/mapping.ts
@@ -0,0 +1,11 @@
+import { Transfer as TransferEvent } from './generated/ERC20Token/ERC20';
+import { Transfer } from './generated/schema';
+
+export function handleTransfer(event: TransferEvent): void {
+  const id = event.transaction.hash.toHex() + '-' + event.logIndex.toString();
+  const entity = new Transfer(id);
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.value = event.params.value;
+  entity.save();
+}
