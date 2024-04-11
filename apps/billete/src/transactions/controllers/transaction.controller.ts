a34945e0c418a212dd89253ea73e599776961bce
LSP20xx <107563633+LSP20xx@users.noreply.github.com>
2024-04-10 21:35:55 -0300

    Added methods to get all transactions and a specific transaction for a user

--- apps/billete/src/transactions/controllers/transaction.controller.ts
+++ apps/billete/src/transactions/controllers/transaction.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { TransactionsService } from '../services/transaction.service';
 
 @Controller('transactions')
 export class TransactionsController {
   constructor(private readonly transactionService: TransactionsService) {}
+
+  @Get(':userId')
+  async getAllTransactions(@Param('userId') userId: string) {
+    return this.transactionService.getAllTransactions(userId);
+  }
+
+  @Get(':userId/:transactionId')
+  async getTransaction(
+    @Param('userId') userId: string,
+    @Param('transactionId') transactionId: string,
+  ) {
+    return this.transactionService.getTransaction(userId, transactionId);
+  }
+}
+
 }
