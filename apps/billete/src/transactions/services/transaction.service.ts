0513efbc8b02557cc4988d7e474f54605d5dca12
LSP20xx <107563633+LSP20xx@users.noreply.github.com>
2024-04-10 21:36:41 -0300

    Updated TransactionsService with methods to get all transactions and a specific transaction for a user

--- apps/billete/src/transactions/services/transaction.service.ts
+++ apps/billete/src/transactions/services/transaction.service.ts
@@ -6,10 +6,32 @@ import { Transaction } from '@prisma/client';
 export class TransactionsService {
   constructor(private databaseService: DatabaseService) {}
 
+  async getAllTransactions(userId: string): Promise<Transaction[]> {
+    return this.databaseService.transaction.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+  }
+
+  async getTransaction(userId: string, transactionId: string): Promise<Transaction | null> {
+    return this.databaseService.transaction.findFirst({
+      where: {
+        AND: [
+          { userId: userId },
+          { id: transactionId },
+        ],
+      },
+    });
+  }
+
   async saveTransaction(txData: any): Promise<Transaction> {
     const transaction = await this.databaseService.transaction.create({
       data: txData,
     });
     return transaction;
+  }
+}
+
   }
 }
